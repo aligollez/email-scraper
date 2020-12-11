6816bcab916a3cb8e48557256957f92e5e61d74e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-12-11 13:55:39 -0500

    Update main.go

--- email-scraper-from-website/main.go
+++ email-scraper-from-website/main.go
@@ -58,7 +58,7 @@ func main() {
 			}
 		}
 	})
-	// // On every a element which has href attribute call callback
+	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		// Print link
