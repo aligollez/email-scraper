c3d336a46b4bfc491398f8221272a05c455f9f04
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-12-28 06:02:03 -0500

    Update main.go

--- email-scraper-from-website/main.go
+++ email-scraper-from-website/main.go
@@ -14,7 +14,7 @@ var allowedDomain = []string{"www.prajwalkoirala.com"}
 
 var emailsAdded map[string]int = make(map[string]int)
 
-const parallelThreads = 2
+const parallelThreads = 25
 
 //Infos info
 type Infos struct {
@@ -61,10 +61,7 @@ func main() {
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
-		// Print link
-		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
+		// Visit link found on page, Only those links are visited which are in AllowedDomains
 		c.Visit(e.Request.AbsoluteURL(link))
 	})
 	// Before making a request print "Visiting ..."
