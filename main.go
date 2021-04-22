6e52376f85510fda8676c7dd15c6ac50e2ff5472
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-04-22 17:43:27 -0400

    Update main.go

--- main.go
+++ main.go
@@ -52,6 +52,7 @@ func main() {
 				infos.data[e.Request.URL.Host] = append(infos.data[e.Request.URL.Host], email)
 			}
 		}
+	})
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
